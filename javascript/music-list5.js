a50533cf8f4b3a70609538d15581e7d3a1766784
ricricardoestevam <ricricardoestevam@gmail.com>
2024-05-13 14:21:27 -0300

    Add files via upload

---
+++ javascript/music-list5.js
@@ -0,0 +1,76 @@
+let allMusic = [
+
+    
+    
+     {
+        name: "2. Por Um Grande Amor",
+        artist: "(1985) Novo País",
+        img: "Capa_NovoPaís1985",
+        src: "02PorUmGrandeAmor"
+    },
+ {
+        name: "3. Gata Real",
+        artist: "(1985) Novo País",
+        img: "Capa_NovoPaís1985",
+        src: "03GataReal"
+    },
+
+ {
+        name: "4. Vibrar",
+        artist: "(1985) Novo País",
+        img: "Capa_NovoPaís1985",
+        src: "04Vibrar"
+    },
+
+ {
+        name: "5. Não Quero Esse Medo",
+        artist: "(1985) Novo País",
+        img: "Capa_NovoPaís1985",
+        src: "05NaoQueroEsseMedo"
+    },
+
+ {
+        name: "6. Oh Meu Amor",
+        artist: "(1985) Novo País",
+        img: "Capa_NovoPaís1985",
+        src: "06OhMeuAmor"
+    },
+
+ {
+        name: "7. Avelã",
+        artist: "(1985) Novo País",
+        img: "Capa_NovoPaís1985",
+        src: "07Avela"
+    },
+
+ {
+        name: "8. Tai Chi Chuan",
+        artist: "(1985) Novo País",
+        img: "Capa_NovoPaís1985",
+        src: "08TaiChiChuan"
+    },
+
+ {
+        name: "9. Será Verdade",
+        artist: "(1985) Novo País",
+        img: "Capa_NovoPaís1985",
+        src: "09SeraVerdade"
+    },
+
+ {
+        name: "10. Novo País",
+        artist: "(1985) Novo País",
+        img: "Capa_NovoPaís1985",
+        src: "10NovoPais"
+    },
+
+ {
+        name: "1. Flash",
+        artist: "(1985) Novo País",
+        img: "Capa_NovoPaís1985",
+        src: "01Flash"
+    },
+
+ 
+    
+]
