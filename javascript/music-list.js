c754db27eb5c5647e51ed723d33269ae5174f3e0
ricricardoestevam <ricricardoestevam@gmail.com>
2024-05-13 09:11:44 -0300

    Add files via upload

---
+++ javascript/music-list.js
@@ -0,0 +1,85 @@
+let allMusic = [
+   
+    {
+        name: "2. Sem Rei",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "02SemRei"
+    },
+ {
+        name: "3. Águias",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "03Aguias"
+    },
+ {
+        name: "4. Maria da Paz",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "04MariaDaPaz"
+    },
+     {
+        name: "5. Água Benta",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "05AguaBenta"
+    },
+
+ {
+        name: "6. Fronteiras",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "06Fronteiras"
+    },
+
+ {
+        name: "7. Te Procuro Lá ",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "07TeProcuroLa"
+    },
+
+ {
+        name: "8. De Banquetes e Jantares",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "08DeBanquetesEJantares"
+    },
+
+ {
+        name: "9. Ruínas de um Sonho",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "09RuinasDeUmSonho"
+    },
+
+ {
+        name: "10. Admirado por Todos",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "10AdmiradoPorTodos"
+    },
+
+ {
+        name: "11. Como Relâmpago no Céu",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "11ComoRelampagoNoCeu"
+    },
+
+ {
+        name: "12. Rasa Calamidade",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "12RasaCalamidade"
+    },
+
+     {
+        name: "1. Que Se Pasa",
+        artist: "(1978) Paralelo 30",
+        img: "Capa_Paralelo30",
+        src: "13QueSePasa"
+    },
+ 
+    
+]
