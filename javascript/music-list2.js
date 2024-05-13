73325a0a3631387de41a72faeafedb1b536dc59d
ricricardoestevam <ricricardoestevam@gmail.com>
2024-05-13 12:25:21 -0300

    Add files via upload

---
+++ javascript/music-list2.js
@@ -0,0 +1,82 @@
+let allMusic = [
+
+     {
+        name: "2. De um Bando",
+        artist: "(1981) Bebeto ALves",
+        img: "Capa_Bebeto_Alves_1981",
+        src: "02DeUmBando"
+    },
+
+     {
+        name: "3. Água",
+        artist: "(1981) Bebeto ALves",
+        img: "Capa_Bebeto_Alves_1981",
+        src: "03Agua"
+    },
+
+     {
+        name: "4. Momento Encantado",
+        artist: "(1981) Bebeto ALves",
+        img: "Capa_Bebeto_Alves_1981",
+        src: "04MomentoEncantado"
+    },
+
+     {
+        name: "5. Moleque do Parque",
+        artist: "(1981) Bebeto ALves",
+        img: "Capa_Bebeto_Alves_1981",
+        src: "05MolequeDoParque"
+    },
+
+     {
+        name: "6. A Mão e o Medo",
+        artist: "(1981) Bebeto ALves",
+        img: "Capa_Bebeto_Alves_1981",
+        src: "06AMaoEOMedo"
+    },
+
+     {
+        name: "7. Fogueirais",
+        artist: "(1981) Bebeto ALves",
+        img: "Capa_Bebeto_Alves_1981",
+        src: "07Fogueirais"
+    },
+
+     {
+        name: "8. Raiar",
+        artist: "(1981) Bebeto ALves",
+        img: "Capa_Bebeto_Alves_1981",
+        src: "08Raiar"
+    },
+
+     {
+        name: "9. Bandeira",
+        artist: "(1981) Bebeto ALves",
+        img: "Capa_Bebeto_Alves_1981",
+        src: "09Bandeira"
+    },
+
+     {
+        name: "10.  Kraft!... Mesmo",
+        artist: "(1981) Bebeto ALves",
+        img: "Capa_Bebeto_Alves_1981",
+        src: "10KraftMesmo"
+    },
+
+     {
+        name: "11. Polvadeira",
+        artist: "(1981) Bebeto ALves",
+        img: "Capa_Bebeto_Alves_1981",
+        src: "11Polvadeira"
+    },
+
+
+     {
+        name: "1. Santana do Uruguay",
+        artist: "(1981) Bebeto ALves",
+        img: "Capa_Bebeto_Alves_1981",
+        src: "01SantanaDoUruguay"
+    },
+ 
+    
+]
