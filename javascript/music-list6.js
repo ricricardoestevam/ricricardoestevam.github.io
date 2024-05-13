cbbc31d7f9a87c105512310f4d08ad3926523551
ricricardoestevam <ricricardoestevam@gmail.com>
2024-05-13 14:40:28 -0300

    Add files via upload

---
+++ javascript/music-list6.js
@@ -0,0 +1,56 @@
+let allMusic = [
+
+    
+    
+     {
+        name: "2. Críticos",
+        artist: "(1987) Pegadas",
+        img: "Capa_1987Pegadas",
+        src: "02Criticos"
+    },
+
+{
+        name: "3. Sobreviventes",
+        artist: "(1987) Pegadas",
+        img: "Capa_1987Pegadas",
+        src: "03Sobreviventes"
+    },
+
+{
+        name: "4. From the Other Side (Hey Man!)",
+        artist: "(1987) Pegadas",
+        img: "Capa_1987Pegadas",
+        src: "04FromTheOtherSide"
+    },
+
+{
+        name: "5. Beirute",
+        artist: "(1987) Pegadas",
+        img: "Capa_1987Pegadas",
+        src: "05Beirute"
+    },
+
+{
+        name: "6. Depois da Chuva",
+        artist: "(1987) Pegadas",
+        img: "Capa_1987Pegadas",
+        src: "06DepoisDaChuva"
+    },
+
+{
+        name: "8. Pegadas",
+        artist: "(1987) Pegadas",
+        img: "Capa_1987Pegadas",
+        src: "08Pegadas"
+    },
+
+ {
+        name: "1. Rasa Calamidade",
+        artist: "(1987) Pegadas",
+        img: "Capa_1987Pegadas",
+        src: "01RasaCalamidade"
+    },
+
+ 
+    
+]
