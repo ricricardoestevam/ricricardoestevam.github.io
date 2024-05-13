f48c38d41763bde357d575df716ad11c8d5fd788
ricricardoestevam <ricricardoestevam@gmail.com>
2024-05-13 14:58:02 -0300

    Add files via upload

---
+++ javascript/music-list7.js
@@ -0,0 +1,77 @@
+let allMusic = [
+
+    
+    
+ {
+        name: "2. Não Adianta Mais",
+        artist: "(1991) Milonga de Paus",
+        img: "Capa_1991MilongadePaus",
+        src: "02NaoAdiantaMais"
+    },
+
+ {
+        name: "3. Bala Perdida",
+        artist: "(1991) Milonga de Paus",
+        img: "Capa_1991MilongadePaus",
+        src: "03BalaPerdida"
+    },
+
+ {
+        name: "4. Desconfiança",
+        artist: "(1991) Milonga de Paus",
+        img: "Capa_1991MilongadePaus",
+        src: "04Desconfianca"
+    },
+
+ {
+        name: "5. Trilha Sonora",
+        artist: "(1991) Milonga de Paus",
+        img: "Capa_1991MilongadePaus",
+        src: "05TrilhaSonora"
+    },
+
+ {
+        name: "6. A Moral da História",
+        artist: "(1991) Milonga de Paus",
+        img: "Capa_1991MilongadePaus",
+        src: "06AMoralDaHistoria"
+    },
+
+ {
+        name: "7. Milonga de Paus",
+        artist: "(1991) Milonga de Paus",
+        img: "Capa_1991MilongadePaus",
+        src: "07MilongaDePaus"
+    },
+
+ {
+        name: "8. Música e Letra",
+        artist: "(1991) Milonga de Paus",
+        img: "Capa_1991MilongadePaus",
+        src: "08MusicaELetra"
+    },
+
+ {
+        name: "9. Bom Dia",
+        artist: "(1991) Milonga de Paus",
+        img: "Capa_1991MilongadePaus",
+        src: "09BomDia"
+    },
+
+ {
+        name: "1. Rasa Calamidade",
+        artist: "(1991) Milonga de Paus",
+        img: "Capa_1991MilongadePaus",
+        src: "01RasaCalamidade"
+    },
+
+ {
+        name: "1. O Espelho",
+        artist: "(1991) Milonga de Paus",
+        img: "Capa_1991MilongadePaus",
+        src: "01OEspelho"
+    },
+
+ 
+    
+]
