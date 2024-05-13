cbd6c2d4b65389f081c684c98fd7a76f20c6323d
ricricardoestevam <ricricardoestevam@gmail.com>
2024-05-13 13:50:22 -0300

    Add files via upload

---
+++ javascript/music-list3.js
@@ -0,0 +1,67 @@
+let allMusic = [
+
+    {
+        name: "2. Mágica",
+        artist: "(1983) Notícia Urgente",
+        img: "Capa_Notícia_Urgente_1983",
+        src: "02Magica"
+    },
+     {
+        name: "3. Chama Criola",
+        artist: "(1983) Notícia Urgente",
+        img: "Capa_Notícia_Urgente_1983",
+        src: "03ChamaCriola"
+    },
+     {
+        name: "4. Amarelua",
+        artist: "(1983) Notícia Urgente",
+        img: "Capa_Notícia_Urgente_1983",
+        src: "04Amarelua"
+    },
+     {
+        name: "5. Notícia Urgente",
+        artist: "(1983) Notícia Urgente",
+        img: "Capa_Notícia_Urgente_1983",
+        src: "05NoticiaUrgente"
+    },
+     {
+        name: "6. Do Azul ao Rosa",
+        artist: "(1983) Notícia Urgente",
+        img: "Capa_Notícia_Urgente_1983",
+        src: "06DoAzulAoRosa"
+    },
+     {
+        name: "7. Meninos",
+        artist: "(1983) Notícia Urgente",
+        img: "Capa_Notícia_Urgente_1983",
+        src: "07Meninos"
+    },
+     {
+        name: "8. Preta",
+        artist: "(1983) Notícia Urgente",
+        img: "Capa_Notícia_Urgente_1983",
+        src: "08Preta"
+    },
+     {
+        name: "9. Sonhos",
+        artist: "(1983) Notícia Urgente",
+        img: "Capa_Notícia_Urgente_1983",
+        src: "09Sonhos"
+    },
+     {
+        name: "10. 433",
+        artist: "(1983) Notícia Urgente",
+        img: "Capa_Notícia_Urgente_1983",
+        src: "10433"
+    },
+
+     {
+        name: "1. AEIOU",
+        artist: "(1983) Notícia Urgente",
+        img: "Capa_Notícia_Urgente_1983",
+        src: "01Aeiou"
+    },
+
+ 
+    
+]
