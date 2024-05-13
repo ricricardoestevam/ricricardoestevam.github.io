feab21dcd0c3fadc649138036d67553b3bc84194
ricricardoestevam <ricricardoestevam@gmail.com>
2024-05-13 14:06:08 -0300

    Add files via upload

---
+++ javascript/music-list4.js
@@ -0,0 +1,20 @@
+let allMusic = [
+
+    
+     {
+        name: "2. Nosso Natal",
+        artist: "(1984) Quando Eu Chegar",
+        img: "Capa_QuandoEuChegar1984",
+        src: "02ONossoNatal"
+    },
+
+     {
+        name: "1. Quando Eu Chegar",
+        artist: "(1984) Quando Eu Chegar",
+        img: "Capa_QuandoEuChegar1984",
+        src: "01QuandoEuChegar"
+    },
+
+ 
+    
+]
