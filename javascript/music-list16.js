4d5f6f92e3ebb066965c97aaea4463fb57a4206b
ricricardoestevam <ricricardoestevam@gmail.com>
2024-06-27 12:47:35 -0300

    Create music-list16.js

---
+++ javascript/music-list16.js
@@ -0,0 +1,77 @@
+let allMusic = [
+
+    
+    
+ {
+        name: "2. Nós Não Vamos Dormir",
+    artist: "(2022) Danço Só",
+        img: "Capa_2022DançoSó",
+        src: "02NosNaoVamosDormir"
+    },
+
+ {
+        name: "3. Tô",
+  artist: "(2022) Danço Só",
+        img: "Capa_2022DançoSó",
+        src: "03To"
+    },
+
+ {
+        name: "4. Tassy",
+artist: "(2022) Danço Só",
+        img: "Capa_2022DançoSó",
+        src: "04Tassy"
+    },
+
+ {
+        name: "5. O Mapa",
+artist: "(2022) Danço Só",
+        img: "Capa_2022DançoSó",
+        src: "05OMapa"
+    },
+
+{
+        name: "6. O Lado Escuro da Lua",
+artist: "(2022) Danço Só",
+        img: "Capa_2022DançoSó",
+        src: "06OLadoEscuroDaLua"
+    },
+ {
+        name: "7. Amor Anfetamina",
+artist: "(2022) Danço Só",
+        img: "Capa_2022DançoSó",
+        src: "07AmorAnfetamina"
+    },
+
+
+ {
+        name: "8. Que Se Pasa",
+    artist: "(2022) Danço Só",
+        img: "Capa_2022DançoSó",
+        src: "08QueSePasa"
+    },
+
+  {
+        name: "9. Uruguaiana",
+artist: "(2022) Danço Só",
+        img: "Capa_2022DançoSó",
+        src: "09Uruguaiana"
+    },
+
+  {
+        name: "10. Tendência",
+artist: "(2022) Danço Só",
+        img: "Capa_2022DançoSó",
+        src: "10Tendencia"
+    },
+  
+ {
+        name: "1. Inimigo Oculto",
+artist: "(2022) Danço Só",
+        img: "Capa_2022DançoSó",
+        src: "01InimigoOculto"
+    },
+
+ 
+    
+]
