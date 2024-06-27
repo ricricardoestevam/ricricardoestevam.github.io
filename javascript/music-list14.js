78d407a2c3587d070d6977a637e11c9555d3930b
ricricardoestevam <ricricardoestevam@gmail.com>
2024-06-27 12:11:16 -0300

    Create music-list14.js

---
+++ javascript/music-list14.js
@@ -0,0 +1,96 @@
+let allMusic = [
+
+    
+    
+ {
+        name: "2. Milonga do Meu Assado",
+    artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "02MilongaDoMeuAssado"
+    },
+
+ {
+        name: "3. Ponteio de Prosa",
+  artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "03PonteioDeProsa"
+    },
+
+ {
+        name: "4. Sobra Cavalo",
+     artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "04SobraCavalo"
+    },
+
+ {
+        name: "5. Lástima",
+        artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "05Lastima"
+    },
+
+ {
+        name: "6. Amoroso",
+          artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "06Amoroso"
+    },
+
+ {
+        name: "7. Milongamento",
+         artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "07Milongamento"
+    },
+
+ {
+        name: "8. Em Cima do Laço",
+       artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "08EmCimaDoLaco"
+    },
+
+
+
+  {
+        name: "10. A Troco de Nada",
+       artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "10ATrocaDeNada"
+    },
+   {
+        name: "11. Fronteira Oeste",
+       artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "11FronteiraOeste"
+    },
+   {
+        name: "13. Atando o Cavalo",
+ artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "13AtandoOCavalo"
+    },
+   {
+        name: "14. Matungos",
+ artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "14Matungos"
+    },
+  {
+        name: "14. Matando a Pau",
+ artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "14Matando a Pau"
+    },
+
+ {
+        name: "1. Camperiando",
+ artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "01Camperiando"
+    },
+
+ 
+    
+]
