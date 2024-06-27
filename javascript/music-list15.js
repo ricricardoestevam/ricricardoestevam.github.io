cf890341b85696db952c9ba35c284cb0da1ba5c3
ricricardoestevam <ricricardoestevam@gmail.com>
2024-06-27 12:38:44 -0300

    Create music-list15.js

---
+++ javascript/music-list15.js
@@ -0,0 +1,107 @@
+let allMusic = [
+
+    
+    
+ {
+        name: "2. Que Bom",
+    artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "02QueBom"
+    },
+
+ {
+        name: "3. Líquido",
+   artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "03Liquido"
+    },
+
+ {
+        name: "4. Te Cuida",
+      artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "04TeCuida"
+    },
+
+ {
+        name: "5. Meia Verdade",
+      artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "05MeiaVerdade"
+    },
+
+{
+        name: "6. Beirute",
+ artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "06Beirute"
+    },
+ {
+        name: "7. Pedra, Pedrinha",
+ artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "07PedraPedrinha"
+    },
+
+
+ {
+        name: "8. A Descoberta",
+      artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "08ADescoberta"
+    },
+
+  {
+        name: "9. Nós Não Vamos Dormir",
+       artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "09NosNaoVamosDormir"
+    },
+
+  {
+        name: "10. Tchau",
+ artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "10Tchau"
+    },
+   {
+        name: "11. Filme B",
+ artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "11FilmeB"
+    },
+   {
+        name: "12. Um Pouco Mais",
+ artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "12UmPoucoMais"
+    },
+   
+ {
+        name: "13. Até Onde o Coração Mandar",
+ artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "13AteOndeOCoracaoMandar"
+    },
+  {
+        name: "14. Noite Azul",
+ artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "14NoiteAzul"
+    },
+  {
+        name: "15. Em Obras",
+ artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "15EmObras"
+    },
+ {
+        name: "1. O Home Invisível",
+ artist: "(2019) O Homem Invisível",
+        img: "Capa2019OHomemInvisivel",
+        src: "01OHomemInvisivel"
+    },
+
+ 
+    
+]
