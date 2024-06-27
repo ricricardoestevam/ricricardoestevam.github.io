4d7a2c8f22580376587ff8aea89698a9f74c0d98
ricricardoestevam <ricricardoestevam@gmail.com>
2024-06-27 11:53:47 -0300

    Create music-list12.js

---
+++ javascript/music-list12.js
@@ -0,0 +1,96 @@
+let allMusic = [
+
+    
+    
+ {
+        name: "2. Literatura Brasileira",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "02LiteraturaBrasileira"
+    },
+
+ {
+        name: "3. Janela do Tempo",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "03JanelaDoTempo"
+    },
+
+ {
+        name: "4. Depois da Chuva",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "04DepoisDaChuva"
+    },
+
+ {
+        name: "5. Vim Vadia",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "05VimVadia"
+    },
+
+ {
+        name: "6. Tempo ao Tempo",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "06TempoAoTempo"
+    },
+
+
+
+ {
+        name: "8. Verniz da Madrugada",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "08VernizDaMadrugada"
+    },
+
+ {
+        name: "9. Mais uma Canção",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "09MaisUmaCancao"
+    },
+
+  {
+        name: "10. Salve-se Quem Souber",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "10SalveSeQuemSouber"
+    },
+   {
+        name: "11. Sinal dos Tempos",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "11SinalDosTempos"
+    },
+   {
+        name: "12. Armadilha",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "12Armadilha"
+    },
+   {
+        name: "13. De um Bando",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "13DeUmBando"
+    },
+  {
+        name: "14. Pedra da Memória",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "14PedraDaMemoria"
+    },
+
+ {
+        name: "1. Pedra da Memória",
+        artist: "(1998) Juntos ao Vivo",
+        img: "Capa_1998JuntosaoVivo",
+        src: "01PedraDaMemoria"
+    },
+
+ 
+    
+]
