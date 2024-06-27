611487b580b6fbcb70d9f8ff0195cdb22c534eae
ricricardoestevam <ricricardoestevam@gmail.com>
2024-06-27 12:07:24 -0300

    Create music-list13.js

---
+++ javascript/music-list13.js
@@ -0,0 +1,96 @@
+let allMusic = [
+
+    
+    
+ {
+        name: "2. Milonga do Meu Assado",
+    artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "02MilongaDoMeuAssado"
+    },
+
+ {
+        name: "3. Ponteio de Prosa",
+   artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "03PonteioDeProsa"
+    },
+
+ {
+        name: "4. Sobra Cavalo",
+    artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "04SobraCavalo"
+    },
+
+ {
+        name: "5. Lástima",
+         artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "05Lastima"
+    },
+
+ {
+        name: "6. Amoroso",
+          artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "06Amoroso"
+    },
+
+ {
+        name: "7. Milongamento",
+          artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "07Milongamento"
+    },
+
+ {
+        name: "8. Em Cima do Laço",
+          artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "08EmCimaDoLaco"
+    },
+
+
+
+  {
+        name: "10. A Troca de Nada",
+          artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "10ATrocaDeNada"
+    },
+   {
+        name: "11. Fronteira Oeste",
+        artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "11FronteiraOeste"
+    },
+   {
+        name: "13. Atando o Cavalo",
+   artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "13AtandoOCavalo"
+    },
+   {
+        name: "14. Matungos",
+       artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "14Matungos"
+    },
+  {
+        name: "14. Matando a Pau",
+         artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "14Matando a Pau"
+    },
+
+ {
+        name: "1. Camperiando",
+        artist: "(1999) Milongamento",
+        img: "Capa_1999Milongamento",
+        src: "01Camperiando"
+    },
+
+ 
+    
+]
