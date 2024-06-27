e42a542b02512dbac15df7dff2fb475612be5486
ricricardoestevam <ricricardoestevam@gmail.com>
2024-06-27 12:15:54 -0300

    Update music-list14.js

--- javascript/music-list14.js
+++ javascript/music-list14.js
@@ -3,92 +3,80 @@ let allMusic = [
     
     
  {
-        name: "2. Milonga do Meu Assado",
+        name: "2. Chão",
     artist: "(2010) Cenas",
         img: "Capa_Cenas",
-        src: "02MilongaDoMeuAssado"
+        src: "02Chao"
     },
 
  {
-        name: "3. Ponteio de Prosa",
+        name: "3. Gauchada",
   artist: "(2010) Cenas",
         img: "Capa_Cenas",
-        src: "03PonteioDeProsa"
+        src: "03Gauchada"
     },
 
  {
-        name: "4. Sobra Cavalo",
+        name: "4. Milonga de Guerra",
      artist: "(2010) Cenas",
         img: "Capa_Cenas",
-        src: "04SobraCavalo"
+        src: "04MilongadeGuerra"
     },
 
  {
-        name: "5. Lástima",
+        name: "5. Boi Barroso",
         artist: "(2010) Cenas",
         img: "Capa_Cenas",
-        src: "05Lastima"
+        src: "05BoiBarroso"
     },
 
- {
-        name: "6. Amoroso",
-          artist: "(2010) Cenas",
-        img: "Capa_Cenas",
-        src: "06Amoroso"
-    },
+
 
  {
-        name: "7. Milongamento",
-         artist: "(2010) Cenas",
-        img: "Capa_Cenas",
-        src: "07Milongamento"
-    },
-
- {
-        name: "8. Em Cima do Laço",
+        name: "8. Corpo Suspenso",
        artist: "(2010) Cenas",
         img: "Capa_Cenas",
-        src: "08EmCimaDoLaco"
+        src: "08CorpoSuspenso"
     },
-
-
 
   {
-        name: "10. A Troco de Nada",
+        name: "9. Desconjunto",
        artist: "(2010) Cenas",
         img: "Capa_Cenas",
-        src: "10ATrocaDeNada"
-    },
-   {
-        name: "11. Fronteira Oeste",
-       artist: "(2010) Cenas",
-        img: "Capa_Cenas",
-        src: "11FronteiraOeste"
-    },
-   {
-        name: "13. Atando o Cavalo",
- artist: "(2010) Cenas",
-        img: "Capa_Cenas",
-        src: "13AtandoOCavalo"
-    },
-   {
-        name: "14. Matungos",
- artist: "(2010) Cenas",
-        img: "Capa_Cenas",
-        src: "14Matungos"
-    },
-  {
-        name: "14. Matando a Pau",
- artist: "(2010) Cenas",
-        img: "Capa_Cenas",
-        src: "14Matando a Pau"
+        src: "09Desconjunto"
     },
 
- {
-        name: "1. Camperiando",
+  {
+        name: "10. Mariazinha vai pro Céu",
+       artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "10MariazinhaVaiProCeu"
+    },
+   {
+        name: "11. Suspensão",
+       artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "11Suspensao"
+    },
+   {
+        name: "12. Breno Mello",
  artist: "(2010) Cenas",
         img: "Capa_Cenas",
-        src: "01Camperiando"
+        src: "12BrenoMello"
+    },
+   {
+        name: "13. O que Vem do Céu",
+ artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "13OQueVemDoCeu"
+    },
+
+
+ {
+        name: "1. Secret Song",
+ artist: "(2010) Cenas",
+        img: "Capa_Cenas",
+        src: "01SecretSong"
     },
 
  
