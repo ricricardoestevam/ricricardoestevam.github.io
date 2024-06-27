f89af4a91265e1f6bd352ffb0b0f3e744119ea30
ricricardoestevam <ricricardoestevam@gmail.com>
2024-06-27 10:44:19 -0300

    Create music-list9.js

---
+++ javascript/music-list9.js
@@ -0,0 +1,108 @@
+let allMusic = [
+
+    
+    
+ {
+        name: "2. Milonga de Paus",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "02MilongaDePaus"
+    },
+
+ {
+        name: "3. Santana do Uruguay",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "03SantanaDoUruguay"
+    },
+
+ {
+        name: "4. Notícia Urgente",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "04NoticiaUrgente"
+    },
+
+ {
+        name: "5. Depois da Chuva",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "05DepoisDaChuva"
+    },
+
+ {
+        name: "6. De Um Bando",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "06DeUmBando"
+    },
+
+ {
+        name: "7. Que Se Pasa",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "07QueSePasa"
+    },
+
+ {
+        name: "8. Bala Perdida",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "08BalaPerdida"
+    },
+
+ {
+        name: "9. Beirute",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "09Beirute"
+    },
+
+  {
+        name: "10. Filme B",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "10FilmeB"
+    },
+   {
+        name: "11. Tai Chi Chuan",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "11TaiChiChuan"
+    },
+   {
+        name: "12. Por Um Grande Amor",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "12PorUmGrandeAmor"
+    },
+   {
+        name: "13. Qunado Eu Chegar",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "13QuandoEuChegar"
+    },
+   {
+        name: "14. Pegadas",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "14Pegadas"
+    },
+   {
+        name: "15. Trilha Sonora",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "15TrilhaSonora"
+    },
+  
+
+ {
+        name: "1. Flash",
+        artist: "(1994) De Aço e Algodão",
+        img: "Capa_1994DeAçoeAlgodão",
+        src: "01Flash"
+    },
+
+ 
+    
+]
