ade2737c071ab139e9fe888be713ec4780f3e262
ricricardoestevam <ricricardoestevam@gmail.com>
2024-06-27 10:31:24 -0300

    Create music-list8.js

---
+++ javascript/music-list8.js
@@ -0,0 +1,113 @@
+let allMusic = [
+
+    
+    
+ {
+        name: "2. Adivinhação",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "02Adivinhacao"
+    },
+
+ {
+        name: "3. Filme B",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "03FilmeB"
+    },
+
+ {
+        name: "4. Toque-me",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "04ToqueMe"
+    },
+
+ {
+        name: "5. Life Style",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "05LifeStyle"
+    },
+
+ {
+        name: "6. Quanto Tempo Mais",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "06QuantoTempoMais"
+    },
+
+ {
+        name: "7. Tum Tum Tum",
+        artist: "(1993) Paisagem",
+        img: "Capa_1991MilongadePaus",
+        src: "07TumTumTum"
+    },
+
+ {
+        name: "8. Noite de Verão no Pampa",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "08NoiteDeVeraoNoPampa"
+    },
+
+ {
+        name: "9. Milonga Sobrenatural",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "09MilongaSobrenatural"
+    },
+
+  {
+        name: "10. Oi Nóis Aqui Traveiz",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "10OiNoisAquiTraveiz"
+    },
+   {
+        name: "11. Verniz",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "11Verniz"
+    },
+   {
+        name: "12. Fragmento de Sinfonia para uma Mosca",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "12FragmentoDeSinfoniaParaUmaMosca"
+    },
+   {
+        name: "13. Paisagem 2",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "13Paisagem2"
+    },
+   {
+        name: "14. A Certeza dos Feiticeiros",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "14ACertezaDosFeiticeiros"
+    },
+   {
+        name: "15. Iolanda de Souza Nunes",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "15IolandaDeSouzaNunes"
+    },
+   {
+        name: "16. Sim Sim Sim",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "10SimSimSim"
+    },
+
+ {
+        name: "1. Paisagem",
+        artist: "(1993) Paisagem",
+        img: "Capa_Paisagem",
+        src: "01Paisagem"
+    },
+
+ 
+    
+]
