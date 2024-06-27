4a632a87e4e9fcd6b02edf748e627a50e909840f
ricricardoestevam <ricricardoestevam@gmail.com>
2024-06-27 11:21:03 -0300

    Create music-list11.js

---
+++ javascript/music-list11.js
@@ -0,0 +1,101 @@
+let allMusic = [
+
+    
+    
+ {
+        name: "2. Milonga pra (Loco)",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "02Milonga Pra (loco)"
+    },
+
+ {
+        name: "3. Mandando Lenha",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "03Mandando Lenha"
+    },
+
+ {
+        name: "4. Do Fundo da Alma",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "04Do Fundo da Alma"
+    },
+
+ {
+        name: "5. Com o Violão na Garupa",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "05Com o Violão na Garupa"
+    },
+
+ {
+        name: "6. Juntando os Gravetos",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "06Juntando os Gravetos"
+    },
+
+ {
+        name: "7. O Sul por Vida",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "07O Sul por Vida"
+    },
+
+ {
+        name: "8. Milonga Amarga",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "08Milonga Amarga"
+    },
+
+ {
+        name: "9. Lavando a Alma",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "09Lavando a Alma"
+    },
+
+  {
+        name: "10. Mostrando a Cara",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "10Mostrando a Cara"
+    },
+   {
+        name: "11. Charla de Tropa",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "11Charla de Tropa"
+    },
+   {
+        name: "12. Com Cisco nos Olhos",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "12Com Cisco nos olhos"
+    },
+   {
+        name: "13. Na Ponta dos Dedos",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "13Na Ponta dos dedos"
+    },
+  {
+        name: "14. Matando a Pau",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "14Matando a Pau"
+    },
+
+ {
+        name: "1. Chamamecero",
+        artist: "(1997) Mandando Lenha",
+        img: "Capa_MandandoLenha",
+        src: "01Chamamecero"
+    },
+
+ 
+    
+]
