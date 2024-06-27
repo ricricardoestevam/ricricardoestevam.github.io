9fc5ba8bdab4b3800fd5117cdc9cf117905dc959
ricricardoestevam <ricricardoestevam@gmail.com>
2024-06-27 11:02:47 -0300

    Create music-list10.js

---
+++ javascript/music-list10.js
@@ -0,0 +1,90 @@
+let allMusic = [
+
+    
+    
+ {
+        name: "2. Fulanos e Sicranos",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "02FulanosESicranos"
+    },
+
+ {
+        name: "3. Tem Coisa na Milonga",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "03TemCoisaNaMilonga"
+    },
+
+ {
+        name: "4. Trova da Casa",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "04TrovaDaCasa"
+    },
+
+ {
+        name: "5. Milonga Abaixo de Mau Tempo",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "05MilongaAbaixoDeMauTempo"
+    },
+
+ {
+        name: "6. Tocando o Cavalo pra Frente",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "06TocandoOCavaloPraFrente"
+    },
+
+ {
+        name: "7. Terra e Gente",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "07TerraEGente"
+    },
+
+ {
+        name: "8. Cismas",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "08Cismas"
+    },
+
+ {
+        name: "9. Gauchada",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "09Gauchada"
+    },
+
+  {
+        name: "10. Canção do Verde",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "10CancaoDoVerde"
+    },
+   {
+        name: "11. Sova de Laço",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "11SovaDeLaco"
+    },
+   {
+        name: "12. Resto de Interior",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "12RestoDeInterior"
+    },
+   
+
+ {
+        name: "1. Milongueando Uns Troços",
+        artist: "(1995) Milongueando Uns Troços",
+        img: "Capa_Milongueando",
+        src: "01MilongueandoUnsTrocos"
+    },
+
+ 
+    
+]
