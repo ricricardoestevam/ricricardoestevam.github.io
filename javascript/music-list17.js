959eac3a47a7ece74bc7ee38319d2d1c4770d24b
ricricardoestevam <ricricardoestevam@gmail.com>
2024-08-09 16:59:32 -0300

    Update music-list17.js

--- javascript/music-list17.js
+++ javascript/music-list17.js
@@ -5,41 +5,41 @@ let allMusic = [
  {
         name: "2. Cura",
     artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "02Cura"
     },
 
  {
         name: "3. Fogueirais",
   artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "03Fogueirais"
     },
 
  {
         name: "4. Lugarzinho",
 artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "04Lugarzinho"
     },
 
  {
         name: "5. Amarelua",
 artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "05Amarelua"
     },
 
 {
         name: "6. Raiar",
 artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "06Raiar"
     },
  {
         name: "7. Vinheta",
 artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "07Vinheta"
     },
 
@@ -47,52 +47,52 @@ artist: "(2020) Salvo 79/80",
  {
         name: "8. De Fogo e Magia",
     artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "08DeFogoeMagia"
     },
 
   {
         name: "9. Ah Essa Cidade",
 artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "09AhEssaCidade"
     },
 
   {
         name: "10. Tendência",
 artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "10Tendencia"
     },
  
    {
         name: "11. Salvo",
 artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "11Salvo"
     },
    {
         name: "12. Palavras de um Novo Dia",
 artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "12PalavrasdeumNovoDia"
     },
    {
         name: "13. De um Bando",
 artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "13DeumBando"
     },
    {
         name: "14. Insônia",
 artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "14Insônia"
     },
     {
         name: "15. Para Eu te Contar",
 artist: "(2020) Salvo 79/80",
-        img: "salvo",
+        img: "capasalvo",
         src: "15Para Eu Te Contar"
     },
  {
