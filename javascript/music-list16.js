d8d364b02274a5d7c6327f886091e64692e109e2
ricricardoestevam <ricricardoestevam@gmail.com>
2024-08-29 23:48:49 -0300

    Update music-list16.js

--- javascript/music-list16.js
+++ javascript/music-list16.js
@@ -4,41 +4,41 @@ let allMusic = [
     
  {
         name: "2. Nós Não Vamos Dormir",
-    artist: "(2022) Danço Só",
+    artist: "(1988) Danço Só",
         img: "Capa_2022DançoSó",
         src: "02NosNaoVamosDormir"
     },
 
  {
         name: "3. Tô",
-  artist: "(2022) Danço Só",
+  artist: "(1988) Danço Só",
         img: "Capa_2022DançoSó",
         src: "03To"
     },
 
  {
         name: "4. Tassy",
-artist: "(2022) Danço Só",
+artist: "(1988) Danço Só",
         img: "Capa_2022DançoSó",
         src: "04Tassy"
     },
 
  {
         name: "5. O Mapa",
-artist: "(2022) Danço Só",
+artist: "(1988) Danço Só",
         img: "Capa_2022DançoSó",
         src: "05OMapa"
     },
 
 {
         name: "6. O Lado Escuro da Lua",
-artist: "(2022) Danço Só",
+artist: "(1988) Danço Só",
         img: "Capa_2022DançoSó",
         src: "06OLadoEscuroDaLua"
     },
  {
         name: "7. Amor Anfetamina",
-artist: "(2022) Danço Só",
+artist: "(1988) Danço Só",
         img: "Capa_2022DançoSó",
         src: "07AmorAnfetamina"
     },
@@ -46,28 +46,28 @@ artist: "(2022) Danço Só",
 
  {
         name: "8. Que Se Pasa",
-    artist: "(2022) Danço Só",
+    artist: "(1988) Danço Só",
         img: "Capa_2022DançoSó",
         src: "08QueSePasa"
     },
 
   {
         name: "9. Uruguaiana",
-artist: "(2022) Danço Só",
+artist: "(1988) Danço Só",
         img: "Capa_2022DançoSó",
         src: "09Uruguaiana"
     },
 
   {
         name: "10. Tendência",
-artist: "(2022) Danço Só",
+artist: "(1988) Danço Só",
         img: "Capa_2022DançoSó",
         src: "10Tendencia"
     },
   
  {
         name: "1. Inimigo Oculto",
-artist: "(2022) Danço Só",
+artist: "(1988) Danço Só",
         img: "Capa_2022DançoSó",
         src: "01InimigoOculto"
     },
