1917dc7f90aa36c6812cb416f50f5fbca8bb6f66
ricricardoestevam <ricricardoestevam@gmail.com>
2024-08-29 23:16:02 -0300

    Create music-list18.js

---
+++ javascript/music-list18.js
@@ -0,0 +1,77 @@
+let allMusic = [
+
+    
+    
+ {
+        name: "2. Céu D'água",
+artist: "(2021) Contraluz",
+   img: "Capa_2021Contraluz",
+        src: "2.CéuD'água"
+    },
+
+ {
+        name: "3. Desconversa",
+artist: "(2021) Contraluz",
+   img: "Capa_2021Contraluz",
+        src: "3.Desconversa"
+    },
+
+ {
+        name: "4. Aque D'el Rei",
+artist: "(2021) Contraluz",
+   img: "Capa_2021Contraluz",
+        src: "4.AqueD'elRei"
+    },
+
+ {
+        name: "5. Um Brilho em Teu Olhar",
+artist: "(2021) Contraluz",
+   img: "Capa_2021Contraluz",
+        src: "5.UmBrilhoemTeuOlhar"
+    },
+
+{
+        name: "6. Preciso Encontrar",
+artist: "(2021) Contraluz",
+  img: "Capa_2021Contraluz",
+        src: "6.PrecisoEncontrar"
+    },
+ {
+        name: "7. Beat",
+artist: "(2021) Contraluz",
+   img: "Capa_2021Contraluz",
+        src: "7.Beat"
+    },
+
+
+ {
+        name: "8. Terra à Vista",
+artist: "(2021) Contraluz",
+   img: "Capa_2021Contraluz",
+        src: "8.TerraàVista"
+    },
+
+  {
+        name: "9. De Déu em Déu",
+artist: "(2021) Contraluz",
+    img: "Capa_2021Contraluz",
+        src: "9.DeDéuemDéu"
+    },
+
+  {
+        name: "10. Aroma Perdido",
+artist: "(2021) Contraluz",
+    img: "Capa_2021Contraluz",
+        src: "10.AromaPerdido"
+    },
+ 
+ {
+        name: "1. Contraluz",
+artist: "(2021) Contraluz",
+        img: "Capa_2021Contraluz",
+        src: "1.Contraluz"
+    },
+
+ 
+    
+]
